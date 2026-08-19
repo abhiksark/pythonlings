1976ee11340101d96420d9e976e21b2f7ddac66f
Nishchal <nishchalv99@gmail.com>
2026-08-19 22:33:12 +0530

    Improve async assertion messages

--- checks/async/async9.py
+++ checks/async/async9.py
@@ -1,4 +1,4 @@
 import asyncio
 
-assert asyncio.run(double_all([1, 2, 3])) == [2, 4, 6]
+assert asyncio.run(double_all([1, 2, 3])) == [2, 4, 6], "double_all() should return [2, 4, 6] for [1, 2, 3]"
 print("async9 ok")
