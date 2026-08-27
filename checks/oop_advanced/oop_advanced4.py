413d4a0317c54c74e85871b019adf1946f71d3a8
ytwei <frobel0520@gmail.com>
2026-08-27 20:17:19 +0800

    fix(curriculum): add actionable advanced OOP assertion messages

--- checks/oop_advanced/oop_advanced4.py
+++ checks/oop_advanced/oop_advanced4.py
@@ -1,3 +1,3 @@
 user = User("Lin")
-assert user.name == "Lin"
+assert user.name == "Lin", f"user.name should be 'Lin', got {user.name!r}"
 print("oop_advanced4 ok")
--- checks/oop_advanced/oop_advanced5.py
+++ checks/oop_advanced/oop_advanced5.py
@@ -1,3 +1,3 @@
 playlist = Playlist(["one", "two", "three"])
-assert len(playlist) == 3
+assert len(playlist) == 3, "len(playlist) should be 3"
 print("oop_advanced5 ok")
